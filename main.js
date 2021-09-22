1e78a5c3116d3af2d221a7f1b74303c90ab7ef9e
efefurkankarakaya <efkdev@outlook.com>
2021-09-22 19:50:11 +0300

    add url import from file

--- main.js
+++ main.js
@@ -1,44 +1,92 @@
+const fs = require("fs");
 const deoxys = require("commander");
 const { processAudio, processVideo } = require("./stream");
 const { createDownloadPath, copyPathToClipboard } = require("./paths");
 
 createDownloadPath();
 
-deoxys.version("1.0.0").description("A high quality Youtube content handler tool");
+deoxys
+  .version("1.0.0")
+  .description("A high quality Youtube content handler tool");
 
 deoxys
   .option("-a, --audio", "Enable audio processing")
   .option("-v --video", "Enable video processing")
-  .option("-c, --clipboard", "Copy the save location to the clipboard (for single processing).")
+  .option(
+    "-c, --clipboard",
+    "Copy the save location to the clipboard (for single processing)."
+  )
   .option("-u, --url <url>", "The url of the video or audio stream")
+  .option("-l, --list <file.txt>", "The file contains list of urls.")
   .parse();
 
 console.log(deoxys.opts());
 
-const { audio, video, clipboard, url } = deoxys.opts();
-console.log(audio, video, clipboard, url);
+const { audio, video, clipboard, url, list } = deoxys.opts();
+console.log(audio, video, clipboard, url, list);
 
 (async () => {
   let filePath = "";
-  if (!url) {
-    console.log("Please provide a url.");
+  if (!url && !list) {
+    console.log("Please provide a url or a file.");
     return;
   }
+
+  if (url && list) {
+    console.log("Please provide only a url or a file, not both.");
+  }
+
+  if (url) {
+    await handleURL(url);
+    return;
+  }
+
+  if (list) {
+    console.log("Clipboard is disabled due to list processing.");
+    await handleFile(list);
+    return;
+  }
+})();
+
+async function handleFile(list) {
+  console.log("Read file");
+  const file = fs.readFileSync(list, "utf8");
+  const data = file.split("\n");
+  console.log(data);
   if (!audio && !video) {
     console.log("Please provide at least one of the stream options.");
     return;
   }
-  if (audio){
+  for (let url of data) {
+    console.log(url);
+    if (audio) {
+      filePath = await processAudio(url);
+    }
+    if (video) {
+      filePath = await processVideo(url);
+    }
+  }
+  console.log("Done.");
+  return;
+}
+
+async function handleURL(url) {
+  if (!audio && !video) {
+    console.log("Please provide at least one of the stream options.");
+    return;
+  }
+  if (audio) {
     filePath = await processAudio(url);
   }
-  if (video){
+  if (video) {
     filePath = await processVideo(url);
   }
-  if (clipboard){
-    if (audio && video) { 
+  if (clipboard) {
+    if (audio && video) {
       console.log("Clipboard is disabled.");
       return;
     }
     copyPathToClipboard(filePath);
+    console.log("Copied to clipboard.");
   }
-})();
+}
